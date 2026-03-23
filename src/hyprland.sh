62952090bd9d7ee243545ad94bbb915ea4bee1cc
maykon-marcos-jr <maykon.marcos.junior.dev@gmail.com>
2026-03-23 06:12:55 -0300

    fixing folder names

--- src/hyprland.sh
+++ src/hyprland.sh
@@ -6,24 +6,8 @@ paru -Sy --needed --noconfirm - < ~/arch-setup/apps/hyprland.list
 
 # creating symlinks for hyprland config
 echo "creating symlinks for hyprland config"
-rm -rf ~/.config/hyprpanel
-ln -sfn ~/arch-setup/config/hyprland/hyprpanel ~/.config/
-ln -sfn ~/arch-setup/config/hyprland/wofi ~/.config/wofi
-mkdir -p ~/.config/hypr
-ln -sfn ~/arch-setup/config/hyprland/conf ~/.config/hypr/conf
-ln -sfn ~/arch-setup/config/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sfn ~/arch-setup/config/hyprland/hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -sfn ~/arch-setup/config/hyprland/hypridle.conf ~/.config/hypr/hypridle.conf
-ln -sfn ~/arch-setup/config/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-mkdir -p ~/.config/hypr/wl-kbptr
-ln -sfn ~/arch-setup/config/hyprland/wl-kbptr.conf ~/.config/hypr/wl-kbptr/config
-mkdir -p ~/.config/sunsetr
-ln -sfn ~/arch-setup/config/hyprland/sunsetr.toml ~/.config/sunsetr/sunsetr.toml
-ln -sfn ~/arch-setup/config/hyprland/sunsetr.service ~/.config/systemd/user/default.target.wants/
-ln -sfn ~/arch-setup/config/hyprland/sunsetr.service ~/.config/systemd/user/graphical-session.target.wants/
-
-
-# enabling services
-echo "enabling hyprland-related services"
-systemctl --user enable --now hypridle
-systemctl --user enable --now sunsetr
+ln -sfn ~/arch-setup/config/hypr ~/.config/
+ln -sfn ~/arch-setup/config/sunsetr ~/.config/
+ln -sfn ~/arch-setup/config/waybar ~/.config/
+ln -sfn ~/arch-setup/config/wl-kbptr/ ~/.config/
+ln -sfn ~/arch-setup/config/wofi ~/.config/
